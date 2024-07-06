beaebc143d2ff01137ec0d22663b37cf4323b0a3
neojarvis <techadmin@iamneo.ai>
2024-07-06 08:51:37 +0000

    Auto-save-commit-06/07/2024--14:21:36

---
+++ authguard_s1_q2/q2/angularapp/src/app/components/dashboard/dashboard.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DashboardComponent } from './dashboard.component';
+
+describe('DashboardComponent', () => {
+  let component: DashboardComponent;
+  let fixture: ComponentFixture<DashboardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DashboardComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DashboardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  fit('Dashboard_component_should_be_created', () => {
+    expect(component).toBeTruthy();
+  });
+});
