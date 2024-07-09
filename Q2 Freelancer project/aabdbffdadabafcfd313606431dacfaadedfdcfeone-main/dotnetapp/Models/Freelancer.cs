c6f55b8bd56a7e254d7157b6e00a08f41397caba
neojarvis <techadmin@iamneo.ai>
2024-07-09 08:24:46 +0000

    Auto-save-commit-09/07/2024--13:54:46

--- Q2 Freelancer project/aabdbffdadabafcfd313606431dacfaadedfdcfeone-main/dotnetapp/Models/Freelancer.cs
+++ Q2 Freelancer project/aabdbffdadabafcfd313606431dacfaadedfdcfeone-main/dotnetapp/Models/Freelancer.cs
@@ -1,12 +1,13 @@
 namespace dotnetapp.Models
 {
-    public class Freelancer
+    public class Project
     {
-        public int FreelancerID { get; set; }
-        public string FreelancerName { get; set; }
-        public string Specialization { get; set; }
-        public decimal CommercialPerHour { get; set; }
-        public string MailID { get; set; }
-        public string ContactNumber { get; set; }
+        public int ProjectID { get; set; } // Primary Key
+        public string ProjectName { get; set; }
+        public string NumberOfModules { get; set; }
+        public string SubmissionDate { get; set; }
+
+        public int FreelancerID { get; set; } // Foreign Key
+        public Freelancer Freelancer { get; set; } // Navigation property
     }
 }
