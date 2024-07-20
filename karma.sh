64416cfb2738211d6d8b58cae12a333cd8b4f67c
neojarvis <techadmin@iamneo.ai>
2024-07-20 11:11:34 +0000

    Auto-save-commit-20/07/2024--16:41:33

--- karma.sh
+++ karma.sh
@@ -9,12 +9,12 @@ if [ -d "/home/coder/project/workspace/angularapp" ]
 then
     echo "project folder present"
     cp /home/coder/project/workspace/karma/karma.conf.js /home/coder/project/workspace/angularapp/karma.conf.js;
-    # checking for application.service.spec.ts component
-    if [ -e "/home/coder/project/workspace/angularapp/src/app/services/application.service.ts" ]
+    # checking for workout.service.spec.ts component
+    if [ -e "/home/coder/project/workspace/angularapp/src/app/services/workout.service.ts" ]
     then
-        cp /home/coder/project/workspace/karma/application.service.spec.ts /home/coder/project/workspace/angularapp/src/app/services/application.service.spec.ts;
+        cp /home/coder/project/workspace/karma/workout.service.spec.ts /home/coder/project/workspace/angularapp/src/app/services/workout.service.spec.ts;
     else
-        echo "Frontend_should_create_application_service FAILED";
+        echo "Frontend_should_create_workout_service FAILED";
     fi
 
     # checking for auth.service.spec.ts component
@@ -25,12 +25,12 @@ then
         echo "Frontend_should_create_auth_service FAILED";
     fi
 
-    # checking for college.service.spec.ts component
-    if [ -e "/home/coder/project/workspace/angularapp/src/app/services/college.service.ts" ]
+    # checking for workoutrequest.service.spec.ts component
+    if [ -e "/home/coder/project/workspace/angularapp/src/app/services/workoutrequest.service.ts" ]
     then
-        cp /home/coder/project/workspace/karma/college.service.spec.ts /home/coder/project/workspace/angularapp/src/app/services/college.service.spec.ts;
+        cp /home/coder/project/workspace/karma/workoutrequest.service.spec.ts /home/coder/project/workspace/angularapp/src/app/services/workoutrequest.service.spec.ts;
     else
-        echo "Frontend_should_create_college_service FAILED";
+        echo "Frontend_should_create_workoutrequest_service FAILED";
     fi
 
     # checking for error.component.spec.ts component
@@ -42,49 +42,49 @@ then
         echo "Frontend_should_contain_wrong_message_in_the_error_component FAILED";
     fi
 
-    # checking for adminaddcollege.component.spec.ts component
-    if [ -d "/home/coder/project/workspace/angularapp/src/app/components/adminaddcollege" ]
+    # checking for adminaddworkout.component.spec.ts component
+    if [ -d "/home/coder/project/workspace/angularapp/src/app/components/adminaddworkout" ]
     then
-        cp /home/coder/project/workspace/karma/adminaddcollege.component.spec.ts /home/coder/project/workspace/angularapp/src/app/components/adminaddcollege/adminaddcollege.component.spec.ts;
+        cp /home/coder/project/workspace/karma/adminaddworkout.component.spec.ts /home/coder/project/workspace/angularapp/src/app/components/adminaddworkout/adminaddworkout.component.spec.ts;
     else
-        echo "Frontend_should_create_adminaddcollege_component FAILED";
-        echo "Frontend_should_contain_create_new_college_heading_in_the_adminaddcollege_component FAILED";
+        echo "Frontend_should_create_adminaddworkout_component FAILED";
+        echo "Frontend_should_contain_create_new_workout_heading_in_the_adminaddworkout_component FAILED";
     fi
 
-    # checking for admineditcollege.component.spec.ts component
-    if [ -d "/home/coder/project/workspace/angularapp/src/app/components/admineditcollege" ]
+    # checking for admineditworkout.component.spec.ts component
+    if [ -d "/home/coder/project/workspace/angularapp/src/app/components/admineditworkout" ]
     then
-        cp /home/coder/project/workspace/karma/admineditcollege.component.spec.ts /home/coder/project/workspace/angularapp/src/app/components/admineditcollege/admineditcollege.component.spec.ts;
+        cp /home/coder/project/workspace/karma/admineditworkout.component.spec.ts /home/coder/project/workspace/angularapp/src/app/components/admineditworkout/admineditworkout.component.spec.ts;
     else
-        echo "Frontend_should_create_admineditcollege_component FAILED";
-        echo "Frontend_should_contain_edit_college_heading_in_the_admineditcollege_component FAILED";
-    fi
-
-    # checking for adminviewcollege.component.spec.ts component
-    if [ -d "/home/coder/project/workspace/angularapp/src/app/components/adminviewcollege" ]
-    then
-        cp /home/coder/project/workspace/karma/adminviewcollege.component.spec.ts /home/coder/project/workspace/angularapp/src/app/components/adminviewcollege/adminviewcollege.component.spec.ts;
-    else
-        echo "Frontend_should_create_adminviewcollege_component FAILED";
-        echo "Frontend_should_contain_colleges_heading_in_the_adminviewcollege_component FAILED";
+        echo "Frontend_should_create_admineditworkout_component FAILED";
+        echo "Frontend_should_contain_edit_workout_heading_in_the_admineditworkout_component FAILED";
     fi
 
     # checking for adminviewfeedback.component.spec.ts component
     if [ -d "/home/coder/project/workspace/angularapp/src/app/components/adminviewfeedback" ]
     then
-        cp /home/coder/project/workspace/karma/adminviewfeedback.component.spec.ts /home/coder/project/workspace/angularapp/src/app/components/adminviewfeedback/managerviewfeedback.component.spec.ts;
+        cp /home/coder/project/workspace/karma/adminviewfeedback.component.spec.ts /home/coder/project/workspace/angularapp/src/app/components/adminviewfeedback/adminviewfeedback.component.spec.ts;
     else
         echo "Frontend_should_create_adminviewfeedback_component FAILED";
         echo "Frontend_should_contain_feedback_details_heading_in_the_adminviewfeedback_component FAILED";
     fi
 
-     # checking for requestedcollege.component.spec.ts component
-    if [ -d "/home/coder/project/workspace/angularapp/src/app/components/requestedcollege" ]
+    # checking for adminviewworkout.component.spec.ts component
+    if [ -d "/home/coder/project/workspace/angularapp/src/app/components/adminviewworkout" ]
     then
-        cp /home/coder/project/workspace/karma/requestedcollege.component.spec.ts /home/coder/project/workspace/angularapp/src/app/components/requestedcollege/requestedcollege.component.spec.ts;
+        cp /home/coder/project/workspace/karma/adminviewworkout.component.spec.ts /home/coder/project/workspace/angularapp/src/app/components/adminviewworkout/adminviewworkout.component.spec.ts;
     else
-        echo "Frontend_should_create_requestedcollege_component FAILED";
-        echo "Frontend_should_contain_college_application_requests_for_approval_heading_in_the_requestedcollege_component FAILED";
+        echo "Frontend_should_create_adminviewworkout_component FAILED";
+        echo "Frontend_should_contain_workouts_heading_in_the_adminviewworkout_component FAILED";
+    fi
+
+     # checking for requestedworkout.component.spec.ts component
+    if [ -d "/home/coder/project/workspace/angularapp/src/app/components/requestedworkout" ]
+    then
+        cp /home/coder/project/workspace/karma/requestedworkout.component.spec.ts /home/coder/project/workspace/angularapp/src/app/components/requestedworkout/requestedworkout.component.spec.ts;
+    else
+        echo "Frontend_should_create_requestedworkout_component FAILED";
+        echo "Frontend_should_contain_workout_requests_for_approval_heading_in_the_requestedworkout_component FAILED";
     fi
 
     # checking for feedback.service.spec.ts component
@@ -101,11 +101,11 @@ then
         cp /home/coder/project/workspace/karma/home.component.spec.ts /home/coder/project/workspace/angularapp/src/app/components/home/home.component.spec.ts;
     else
         echo "Frontend_should_create_home_component FAILED";
-        echo "Frontend_should_contain_globaledconnect_heading_in_the_home_component FAILED";
+        echo "Frontend_should_contain_fitness_tracker_heading_in_the_home_component FAILED";
     fi
 
     # checking for useraddfeedback.component.spec.ts component
-    if [ -d "/useraddfeedback/coder/project/workspace/angularapp/src/app/components/useraddfeedback" ]
+    if [ -d "/home/coder/project/workspace/angularapp/src/app/components/useraddfeedback" ]
     then
         cp /home/coder/project/workspace/karma/useraddfeedback.component.spec.ts /home/coder/project/workspace/angularapp/src/app/components/useraddfeedback/useraddfeedback.component.spec.ts;
     else
@@ -128,7 +128,7 @@ then
         cp /home/coder/project/workspace/karma/navbar.component.spec.ts /home/coder/project/workspace/angularapp/src/app/components/navbar/navbar.component.spec.ts;
     else
         echo "Frontend_should_create_navbar_component FAILED";
-        echo "Frontend_should_contain_globaledconnect_heading_in_the_navbar_component FAILED";
+        echo "Frontend_should_contain_fitness_tracker_heading_in_the_navbar_component FAILED";
     fi
 
     # checking for adminnav.component.spec.ts component
@@ -136,7 +136,7 @@ then
     then
         cp /home/coder/project/workspace/karma/adminnav.component.spec.ts /home/coder/project/workspace/angularapp/src/app/components/adminnav/adminnav.component.spec.ts;
     else
-        echo "Frontent_should_create_adminnav_component FAILED";
+        echo "Frontend_should_create_adminnav_component FAILED";
     fi
 
     # checking for registration.component.spec.ts component
@@ -148,22 +148,22 @@ then
         echo "Frontend_should_contain_registration_heading_in_the_registration_component FAILED";
     fi
 
-    # checking for userapplicationform.component.spec.ts component
-    if [ -d "/home/coder/project/workspace/angularapp/src/app/components/userapplicationform" ]
+    # checking for userappliedworkout.component.spec.ts component
+    if [ -d "/home/coder/project/workspace/angularapp/src/app/components/userappliedworkout" ]
     then
-        cp /home/coder/project/workspace/karma/userapplicationform.component.spec.ts /home/coder/project/workspace/angularapp/src/app/components/userapplicationform/userapplicationform.component.spec.ts;
+        cp /home/coder/project/workspace/karma/userappliedworkout.component.spec.ts /home/coder/project/workspace/angularapp/src/app/components/userappliedworkout/userappliedworkout.component.spec.ts;
     else
-        echo "Frontend_should_create_userapplicationform_component FAILED";
-        echo "Frontend_should_contain_college_application_form_heading_in_the_userapplicationform_component FAILED";
+        echo "Frontend_should_create_userappliedworkout_component FAILED";
+        echo "Frontend_should_contain_applied_workouts_heading_in_the_userappliedworkout_component FAILED";
     fi
 
-    # checking for userappliedcollege.component.spec.ts component
-    if [ -d "/home/coder/project/workspace/angularapp/src/app/components/userappliedcollege" ]
+    # checking for userviewworkout.component.spec.ts component
+    if [ -d "/home/coder/project/workspace/angularapp/src/app/components/userviewworkout" ]
     then
-        cp /home/coder/project/workspace/karma/userappliedcollege.component.spec.ts /home/coder/project/workspace/angularapp/src/app/components/userappliedcollege/userappliedcollege.component.spec.ts;
+        cp /home/coder/project/workspace/karma/userviewworkout.component.spec.ts /home/coder/project/workspace/angularapp/src/app/components/userviewworkout/userviewworkout.component.spec.ts;
     else
-        echo "Frontend_should_create_userappliedcollege_component FAILED";
-        echo "Frontend_should_contain_applied_colleges_heading_in_the_userappliedcollege_component FAILED";
+        echo "Frontend_should_create_userviewworkout_component FAILED";
+        echo "Frontend_should_contain_available_workouts_heading_in_the_userviewworkout_component FAILED";
     fi
 
     # checking for usernav.component.spec.ts component
@@ -174,20 +174,20 @@ then
         echo "Frontend_should_create_usernav_component FAILED";
     fi
 
-    # checking for userviewcollege.component.spec.ts component
-    if [ -d "/home/coder/project/workspace/angularapp/src/app/components/userviewcollege" ]
+    # checking for userworkoutform.component.spec.ts component
+    if [ -d "/home/coder/project/workspace/angularapp/src/app/components/userworkoutform" ]
     then
-        cp /home/coder/project/workspace/karma/userviewcollege.component.spec.ts /home/coder/project/workspace/angularapp/src/app/components/userviewcollege/userviewcollege.component.spec.ts;
+        cp /home/coder/project/workspace/karma/userworkoutform.component.spec.ts /home/coder/project/workspace/angularapp/src/app/components/userworkoutform/userworkoutform.component.spec.ts;
     else
-        echo "Frontend_should_create_userviewcollege_component FAILED";
-        echo "Frontend_should_contain_available_colleges_heading_in_the_userviewcollege_component FAILED";
+        echo "Frontend_should_create_userworkoutform_component FAILED";
+        echo "Frontend_should_contain_workout_application_form_heading_in_the_userworkoutform_component FAILED";
     fi
 
     # checking for userviewfeedback.component.spec.ts component
     if [ -d "/home/coder/project/workspace/angularapp/src/app/components/userviewfeedback" ]
     then
         cp /home/coder/project/workspace/karma/userviewfeedback.component.spec.ts /home/coder/project/workspace/angularapp/src/app/components/userviewfeedback/userviewfeedback.component.spec.ts;
-    else
+    else 
         echo "Frontend_should_create_userviewfeedback_component FAILED";
         echo "Frontend_should_contain_my_feedback_heading_in_the_userviewfeedback_component FAILED";
     fi
@@ -202,39 +202,5 @@ then
         npm test
     fi 
 else   
-    echo "Frontend_should_be_create_project_service FAILED";
-    echo "Frontend_should_create_auth_service FAILED";
-    echo "Frontend_should_create_errorcomponent FAILED";
-    echo "Frontend_should_contain_wrong_message_in_the_errorcomponent FAILED";
-    echo "Frontend_should_create_manager_edit_project_component FAILED";
-    echo "Frontend_should_contain_edit_project_heading_in_the_manager_edit_project_component FAILED";
-    echo "Frontend_should_create_analyst_view_project_component FAILED";
-    echo "Frontend_should_contain_projects_heading_in_the_analyst_view_project_component FAILED";
-    echo "Frontend_should_create_analyst_view_requirement_component FAILED";
-    echo "Frontend_should_contain_view_requirements_heading_in_the_analyst_view_requirement_component FAILED";
-    echo "Frontend_should_create_manager_view_feedback_component FAILED";
-    echo "Frontend_should_contain_feedback_details_heading_in_the_manager_view_feedback_component FAILED";
-    echo "Frontend_should_create_manager_view_project_component FAILED";
-    echo "Frontend_should_contain_projects_heading_in_the_manager_view_project_component FAILED";
-    echo "Frontend_should_create_feedback_service FAILED";
-    echo "Frontend_should_create_home_component FAILED";
-    echo "Frontend_should_contain_req_bridge_pro_heading_in_the_home_component FAILED";
-    echo "Frontend_should_create_manager_view_requirement_component FAILED";
-    echo "Frontend_should_contain_all_project_requirements_heading_in_the_manager_view_requirement_component FAILED";
-    echo "Frontend_should_create_login_component FAILED";
-    echo "Frontend_should_contain_login_heading_in_the_login_component FAILED";
-    echo "Frontend_should_create_navbar_component FAILED";
-    echo "Frontend_should_contain_req_bridge_pro_heading_in_the_navbar_component FAILED";
-    echo "Frontend_should_create_manager_nav_component FAILED";
-    echo "Frontend_should_create_registration_component FAILED";
-    echo "Frontend_should_contain_registration_heading_in_the_registration_component FAILED";
-    echo "Frontend_should_create_analystaddfeedback_component FAILED";
-    echo "Frontend_should_contain_add_feedback_heading_in_the_analystaddfeedback_component FAILED";
-    echo "Frontend_should_create_manager_add_project_component FAILED";
-    echo "Frontend_should_contain_create_new_project_heading_in_the_manager_add_project_component FAILED";
-    echo "Frontend_should_create_analystnav_component FAILED";
-    echo "Frontend_should_create_analyst_view_feedback_component FAILED";
-    echo "Frontend_should_contain_my_feedback_heading_in_the_analyst_view_feedback_component FAILED";
-    echo "Frontend_should_create_analyst_add_requirement_component FAILED";
-    echo "Frontend_should_contain_add_requirement_heading_in_the_analyst_add_requirement_component FAILED";
+    
 fi
