7ef7cb9d0dadd74f9add4c8aa51caf68e3b96568
neojarvis <techadmin@iamneo.ai>
2024-07-30 16:04:46 +0000

    Auto-save-commit-30/07/2024--21:34:46

--- dotnetapp/Models/Participant.cs
+++ dotnetapp/Models/Participant.cs
@@ -1,26 +1,49 @@
+// using System;
+// using System.ComponentModel.DataAnnotations;
+
+// namespace dotnetapp.Models
+// {
+//     public class Participant
+//     {
+//         public int ParticipantID { get; set; }
+
+//         [Required(ErrorMessage = "Name is required")]
+//         public string Name { get; set; }
+
+//         [Required(ErrorMessage = "Email is required")]
+//         [EmailAddress(ErrorMessage = "Invalid Email Address")]
+//         public string Email { get; set; }
+
+//         [Required(ErrorMessage = "PhoneNumber is required")]
+//         [Phone(ErrorMessage = "Invalid Phone Number")]
+//         public string PhoneNumber { get; set; }
+
+//         [Required(ErrorMessage = "HistoricalTourID is required")]
+//         public int HistoricalTourID { get; set; }
+
+//         public virtual HistoricalTour? HistoricalTour { get; set; }
+//     }
+// }
+
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace dotnetapp.Models
 {
-    public class Participant
+    public class Movie
     {
-        public int ParticipantID { get; set; }
+        public int MovieID { get; set; }
 
-        [Required(ErrorMessage = "Name is required")]
-        public string Name { get; set; }
+        [Required(ErrorMessage = "Title is required")]
+        public string Title { get; set; }
 
-        [Required(ErrorMessage = "Email is required")]
-        [EmailAddress(ErrorMessage = "Invalid Email Address")]
-        public string Email { get; set; }
+        [Required(ErrorMessage = "Director is required")]
+        public string Director { get; set; }
 
-        [Required(ErrorMessage = "PhoneNumber is required")]
-        [Phone(ErrorMessage = "Invalid Phone Number")]
-        public string PhoneNumber { get; set; }
+        [Required(ErrorMessage = "ReleaseDate is required")]
+        public DateTime ReleaseDate { get; set; }
 
-        [Required(ErrorMessage = "HistoricalTourID is required")]
-        public int HistoricalTourID { get; set; }
-
-        public virtual HistoricalTour? HistoricalTour { get; set; }
+        public virtual ICollection<MovieReview>? Reviews { get; set; }
     }
 }
