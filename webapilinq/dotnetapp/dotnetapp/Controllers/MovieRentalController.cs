0356802c23ed05a43c98baada1824a635c742599
neojarvis <techadmin@iamneo.ai>
2024-08-05 07:04:35 +0000

    Auto-save-commit-05/08/2024--12:34:36

--- webapilinq/dotnetapp/dotnetapp/Controllers/MovieRentalController.cs
+++ webapilinq/dotnetapp/dotnetapp/Controllers/MovieRentalController.cs
@@ -7,96 +7,81 @@ using dotnetapp.Models;
 
 namespace dotnetapp.Controllers
 {
-[ApiController]
-[Route("api/[controller]")]
-public class MovieRentalController : ControllerBase
-{
-    private readonly ApplicationDbContext _context;
-
-    public MovieRentalController(ApplicationDbContext context)
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MovieRentalController : ControllerBase
     {
-        _context = context;
-    }
+        private readonly ApplicationDbContext _context;
 
-    [HttpGet("DisplayMoviesForCustomer/{customerId}")]
-    public async Task<ActionResult<IEnumerable<Movie>>> DisplayMoviesForCustomer(int customerId)
-    {
-        var movies = await _context.Movies
-            .Where(m => m.CustomerId == customerId)
-            .ToListAsync();
-
-        if (!movies.Any())
+        public MovieRentalController(ApplicationDbContext context)
         {
-            return NotFound();
+            _context = context;
         }
 
-        return Ok(movies);
-    }
-
-    // [HttpPost("AddMovie")]
-    // public async Task<ActionResult<Movie>> AddMovie([FromBody] Movie movie)
-    // {
-    //     if (!ModelState.IsValid)
-    //     {
-    //         return BadRequest(ModelState);
-    //     }
-
-    //     _context.Movies.Add(movie);
-    //     await _context.SaveChangesAsync();
-
-    //     return CreatedAtAction(nameof(AddMovie), new { id = movie.Id }, movie);
-    // }
-
-    [HttpPost("AddMovie")]
-public async Task<ActionResult<Movie>> AddMovie([FromBody] Movie movie)
-{
-    if (!ModelState.IsValid)
-    {
-        return BadRequest(ModelState);
-    }
-
-    // Fetch the customer based on the customerId
-    var customer = await _context.Customers.FindAsync(movie.CustomerId);
-    if (customer == null)
-    {
-        return NotFound(new { Message = "Customer not found" });
-    }
-
-    // Associate the fetched customer with the movie
-    movie.Customer = customer;
-
-    _context.Movies.Add(movie);
-    await _context.SaveChangesAsync();
-
-    // Include the customer when returning the movie
-    var addedMovie = await _context.Movies
-        .Include(m => m.Customer) // Include the Customer navigation property
-        .SingleOrDefaultAsync(m => m.Id == movie.Id);
-
-    return CreatedAtAction(nameof(AddMovie), new { id = addedMovie.Id }, addedMovie);
-}
-
-
-    [HttpGet("DisplayAllMovies")]
-    public async Task<ActionResult<IEnumerable<Movie>>> DisplayAllMovies()
-    {
-        var movies = await _context.Movies.ToListAsync();
-        return Ok(movies);
-    }
-
-    [HttpGet("SearchMoviesByTitle/{query}")]
-    public async Task<ActionResult<IEnumerable<Movie>>> SearchMoviesByTitle(string query)
-    {
-        var movies = await _context.Movies
-            .Where(m => m.Title.Contains(query))
-            .ToListAsync();
-
-        if (!movies.Any())
+        [HttpGet("DisplayMoviesForCustomer/{customerId}")]
+        public async Task<ActionResult<IEnumerable<Movie>>> DisplayMoviesForCustomer(int customerId)
         {
-            return NotFound();
+            var movies = await _context.Movies
+                .Where(m => m.CustomerId == customerId)
+                .ToListAsync();
+
+            if (!movies.Any())
+            {
+                return NotFound();
+            }
+
+            return Ok(movies);
         }
 
-        return Ok(movies);
+        [HttpPost("AddMovie")]
+        public async Task<ActionResult<Movie>> AddMovie([FromBody] Movie movie)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            // Fetch the customer based on the customerId
+            var customer = await _context.Customers.FindAsync(movie.CustomerId);
+            if (customer == null)
+            {
+                return NotFound(new { Message = "Customer not found" });
+            }
+
+            // Associate the fetched customer with the movie
+            movie.Customer = customer;
+
+            _context.Movies.Add(movie);
+            await _context.SaveChangesAsync();
+
+            // Include the customer when returning the movie
+            var addedMovie = await _context.Movies
+                .Include(m => m.Customer) // Include the Customer navigation property
+                .SingleOrDefaultAsync(m => m.Id == movie.Id);
+
+            return CreatedAtAction(nameof(DisplayMoviesForCustomer), new { customerId = addedMovie.CustomerId }, addedMovie);
+        }
+
+        [HttpGet("DisplayAllMovies")]
+        public async Task<ActionResult<IEnumerable<Movie>>> DisplayAllMovies()
+        {
+            var movies = await _context.Movies.ToListAsync();
+            return Ok(movies);
+        }
+
+        [HttpGet("SearchMoviesByTitle/{query}")]
+        public async Task<ActionResult<IEnumerable<Movie>>> SearchMoviesByTitle(string query)
+        {
+            var movies = await _context.Movies
+                .Where(m => m.Title.Contains(query))
+                .ToListAsync();
+
+            if (!movies.Any())
+            {
+                return NotFound();
+            }
+
+            return Ok(movies);
+        }
     }
- }
 }
