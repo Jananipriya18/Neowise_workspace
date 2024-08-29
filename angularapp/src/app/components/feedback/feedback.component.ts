53274aa14d93645503eb76a61171ea7aaf10f2bf
neojarvis <techadmin@iamneo.ai>
2024-08-29 17:29:48 +0000

    Auto-save-commit-29/08/2024--22:59:48

--- angularapp/src/app/components/feedback/feedback.component.ts
+++ angularapp/src/app/components/feedback/feedback.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-feedback',
@@ -12,20 +13,53 @@ export class FeedbackComponent {
 
   feedbackList: any[] = []; // Temporary storage for feedback data
 
-  submitFeedback() {
-    // Create a feedback object with the submitted data
-    const feedback = {
-      passengerName: this.passengerName,
-      rating: this.rating,
-      comment: this.comment
-    };
+  submitFeedback(form: NgForm) {
+    if (form.valid) {
+      // Create a feedback object with the submitted data
+      const feedback = {
+        passengerName: this.passengerName,
+        rating: this.rating,
+        comment: this.comment
+      };
 
-    // Add the feedback to the list (temporary storage)
-    this.feedbackList.push(feedback);
+      // Add the feedback to the list (temporary storage)
+      this.feedbackList.push(feedback);
 
-    // Clear the form after submission
-    this.passengerName = '';
-    this.rating = 1;
-    this.comment = '';
+      // Clear the form after submission
+      form.resetForm(); // Reset form fields and set default values
+    }
   }
 }
+
+
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-feedback',
+//   templateUrl: './feedback.component.html',
+//   styleUrls: ['./feedback.component.css']
+// })
+// export class FeedbackComponent {
+//   passengerName: string = '';
+//   rating: number = 5;
+//   comment: string = '';
+
+//   feedbackList: any[] = []; // Temporary storage for feedback data
+
+//   submitFeedback() {
+//     // Create a feedback object with the submitted data
+//     const feedback = {
+//       passengerName: this.passengerName,
+//       rating: this.rating,
+//       comment: this.comment
+//     };
+
+//     // Add the feedback to the list (temporary storage)
+//     this.feedbackList.push(feedback);
+
+//     // Clear the form after submission
+//     this.passengerName = '';
+//     this.rating = 1;
+//     this.comment = '';
+//   }
+// }
