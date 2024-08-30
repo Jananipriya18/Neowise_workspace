d40065b1ea216585b4ac45f1dd6fc00ba202b21a
neojarvis <techadmin@iamneo.ai>
2024-08-30 05:16:34 +0000

    Auto-save-commit-30/08/2024--10:46:33

---
+++ angularapp/src/app/models/flight.model.ts
@@ -0,0 +1,15 @@
+export interface Flight {
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    availableSeats: number;
+    departureDate: string;
+    arrivalDate: string;
+    departureLocation: string;
+    arrivalLocation: string;
+    airline: string;
+    duration: string;
+    imageSrc: string;
+  }
+  
