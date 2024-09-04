bc7abc41bf9fe986b8a467e8ca09aafee2b545c7
neojarvis <techadmin@iamneo.ai>
2024-09-04 10:41:41 +0000

    Auto-save-commit-04/09/2024--16:11:41

--- StudentCourseRegistration/dotnetapp/Models/Student.cs
+++ StudentCourseRegistration/dotnetapp/Models/Student.cs
@@ -7,6 +7,6 @@ namespace dotnetapp.Models
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
         public string Department {get;set;}
-        public ICollection<Course> Courses { get; set; }
+        public ICollection<Course>? Courses { get; set; }
     }
 }
