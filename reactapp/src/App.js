2de4942843a24ccf4e99b160c40bf8274640f7e7
neojarvis <techadmin@iamneo.ai>
2024-09-09 08:02:42 +0000

    Auto-save-commit-09/09/2024--13:32:43

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import PerfumeList from './components/PerfumeDisplay';
-import './index'
+import PerfumeDisplay from './components/PerfumeDisplay'; // Make sure to import from the correct path
+import './assets/css/index.css';
+
 function App() {
-  // Perfume data
+  // Perfume data with fragrance
   const perfumes = [
-    { name: 'Chanel No. 5', brand: 'Chanel', price: 120 },
-    { name: 'Acqua di Gio', brand: 'Giorgio Armani', price: 95 },
-    { name: 'Dior Sauvage', brand: 'Christian Dior', price: 110 },
-    { name: 'Black Orchid', brand: 'Tom Ford', price: 150 }
+    { name: 'Chanel No. 5', brand: 'Chanel', fragrance: 'Floral', price: 120 },
+    { name: 'Acqua di Gio', brand: 'Giorgio Armani', fragrance: 'Citrus', price: 95 },
+    { name: 'Dior Sauvage', brand: 'Christian Dior', fragrance: 'Woody', price: 110 },
+    { name: 'Black Orchid', brand: 'Tom Ford', fragrance: 'Oriental', price: 150 }
   ];
 
   return (
     <div className="app-container">
       <h1 className="heading">Perfume Collection</h1>
-      <PerfumeList perfumes={perfumes} />
+      <PerfumeDisplay perfumes={perfumes} />
     </div>
   );
 }
--- reactapp/src/components/PerfumeDisplay.jsx
+++ reactapp/src/components/PerfumeDisplay.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 
-const PerfumeList = ({ perfumes }) => {
+const PerfumeDisplay = ({ perfumes }) => {
   return (
     <div className="perfume-list-container">
       {perfumes.map((perfume, index) => (
         <div key={index} className="perfume-item">
           <h4 className="perfume-name">{perfume.name}</h4>
           <p className="perfume-brand">Brand: {perfume.brand}</p>
-          <p className="perfume-price">Price: ${perfume.price}</p>
+          <p className="perfume-fragrance">Fragrance: {perfume.fragrance}</p>
+          <p className="perfume-price">Price: ${perfume.price}</p> ``
         </div>
       ))}
     </div>
   );
 };
 
-export default PerfumeList;
+export default PerfumeDisplay;
