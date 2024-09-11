3216ad4bd1dcd3d69bfb19671b834b9e63153002
neojarvis <techadmin@iamneo.ai>
2024-09-11 06:59:08 +0000

    Auto-save-commit-11/09/2024--12:29:08

--- EventManagementSystem/dotnetapp/Models/Event.cs
+++ EventManagementSystem/dotnetapp/Models/Event.cs
@@ -6,7 +6,7 @@ namespace dotnetapp.Models
     {
         public int EventId { get; set; }  // Primary key
         public string Name { get; set; }  // Name of the event
-        public DateTime EventDate { get; set; }  // Date and time of the event
+        public string    { get; set; }  // Date and time of the event
         public string Location { get; set; }  // Location of the event
 
         // Navigation property for attendees of the event
