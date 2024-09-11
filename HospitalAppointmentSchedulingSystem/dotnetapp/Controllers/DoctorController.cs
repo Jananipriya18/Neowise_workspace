d082bc118708bf6253bc8374c3e0ad4412a4d8a3
neojarvis <techadmin@iamneo.ai>
2024-09-11 11:17:58 +0000

    Auto-save-commit-11/09/2024--16:47:58

--- HospitalAppointmentSchedulingSystem/dotnetapp/Controllers/DoctorController.cs
+++ HospitalAppointmentSchedulingSystem/dotnetapp/Controllers/DoctorController.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using dotnetapp.Data;
@@ -48,5 +52,83 @@ namespace dotnetapp.Controllers
 
             return doctor;
         }
+
+        // GET: api/Doctor/sortedbyfee
+        [HttpGet("sortedbyfee")]
+        public async Task<ActionResult<IEnumerable<Doctor>>> GetDoctorsSortedByFeeDescending()
+        {
+            var doctors = await _context.Doctors
+                .OrderByDescending(d => ConvertToDecimal(d.DoctorFee)) // Assumes DoctorFee is a string representing a decimal
+                .Include(d => d.Appointments)
+                .ToListAsync();
+
+            return Ok(doctors);
+        }
+
+        // Utility method to convert DoctorFee to decimal
+        private decimal ConvertToDecimal(string fee)
+        {
+            // Handle conversion; assume fee is in a valid decimal format
+            if (decimal.TryParse(fee, out var result))
+            {
+                return result;
+            }
+
+            // Return a default value or handle invalid conversion
+            return 0m;
+        }
     }
 }
+
+// using Microsoft.AspNetCore.Mvc;
+// using Microsoft.EntityFrameworkCore;
+// using dotnetapp.Data;
+// using dotnetapp.Models;
+
+// namespace dotnetapp.Controllers
+// {
+//     [Route("api/[controller]")]
+//     [ApiController]
+//     public class DoctorController : ControllerBase
+//     {
+//         private readonly ApplicationDbContext _context;
+
+//         public DoctorController(ApplicationDbContext context)
+//         {
+//             _context = context;
+//         }
+
+//         // POST: api/Doctor
+//         [HttpPost]
+//         public async Task<ActionResult<Doctor>> CreateDoctor([FromBody] Doctor doctor)
+//         {
+//             _context.Doctors.Add(doctor);
+//             await _context.SaveChangesAsync();
+
+//             return CreatedAtAction(nameof(GetDoctor), new { id = doctor.DoctorId }, doctor);
+//         }
+
+//         // GET: api/Doctor
+//         [HttpGet]
+//         public async Task<ActionResult<IEnumerable<Doctor>>> GetDoctors()
+//         {
+//             return await _context.Doctors.Include(d => d.Appointments).ToListAsync();
+//         }
+
+//         // GET: api/Doctor/5
+//         [HttpGet("{id}")]
+//         public async Task<ActionResult<Doctor>> GetDoctor(int id)
+//         {
+//             var doctor = await _context.Doctors
+//                 .Include(d => d.Appointments)
+//                 .FirstOrDefaultAsync(d => d.DoctorId == id);
+
+//             if (doctor == null)
+//             {
+//                 return NotFound();
+//             }
+
+//             return doctor;
+//         }
+//     }
+// }
