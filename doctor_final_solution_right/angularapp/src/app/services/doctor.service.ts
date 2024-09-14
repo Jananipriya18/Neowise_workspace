06c877c428641c92ff9cdea2216b012109888c22
neojarvis <techadmin@iamneo.ai>
2024-09-14 14:13:23 +0000

    Auto-save-commit-14/09/2024--19:43:23

---
+++ doctor_final_solution_right/angularapp/src/app/services/doctor.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Doctor } from '../model/doctor.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DoctorService {
+  public backendUrl = 'https://ide-cbfcedbeceaadb316869310facbfbbffefbfone.premiumproject.examly.io/proxy/3001/doctors'; 
+
+  constructor(private http: HttpClient) { }
+
+  getDoctors(): Observable<Doctor[]> {
+    return this.http.get<Doctor[]>(this.backendUrl);
+  }
+
+  addDoctor(doctor: Doctor): Observable<Doctor> {
+    return this.http.post<Doctor>(this.backendUrl, doctor);
+  }
+
+  getDoctorById(id: number): Observable<Doctor> {
+    const url = `${this.backendUrl}/${id}`;
+    return this.http.get<Doctor>(url);
+  }
+  updateDoctor(id: number, doctor: Doctor): Observable<Doctor> {
+    const url = `${this.backendUrl}/${id}`;
+    return this.http.put<Doctor>(url, doctor);
+  }
+
+  deleteDoctor(id: number): Observable<void> {
+    const url = `${this.backendUrl}/${id}`;
+    return this.http.delete<void>(url);
+  }
+}
