c2b929bcb54fe71f0989df8a4a6868e1450c188a
neojarvis <techadmin@iamneo.ai>
2024-09-17 09:38:01 +0000

    Auto-save-commit-17/09/2024--15:08:01

--- MyComics/angularapp/src/app/model/comic.model.ts
+++ MyComics/angularapp/src/app/model/comic.model.ts
@@ -1,10 +1,21 @@
-export interface Doctor {
-    id?: number;
-    name: string;
-    age: number;
-    specialization: string;
-    department: string;
-    contactNumber: string;
-  }
+// export interface Doctor {
+//     id?: number;
+//     name: string;
+//     age: number;
+//     specialization: string;
+//     department: string;
+//     contactNumber: string;
+//   }
   
 
+//   export interface Comic {
+//     id?: number;                   // Unique identifier for the comic
+//     title: string;                // Title of the comic book
+//     author: string;               // Author(s) of the comic book
+//     series: string;               // Series name to which the comic belongs
+//     publisher: string;            // Publisher of the comic book
+//     publicationDate: Date;        // Date when the comic book was published
+//     genre: string;                // Genre or category of the comic book
+//     description?: string;         // Optional brief description or synopsis
+//   }
+  
