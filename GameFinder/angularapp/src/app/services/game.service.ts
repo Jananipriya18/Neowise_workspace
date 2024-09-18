d36a60bf87e01ddccb034788ecb87a96d13d4310
neojarvis <techadmin@iamneo.ai>
2024-09-18 17:02:41 +0000

    Auto-save-commit-18/09/2024--22:32:40

--- GameFinder/angularapp/src/app/services/game.service.ts
+++ GameFinder/angularapp/src/app/services/game.service.ts
@@ -4,32 +4,38 @@ import { Observable } from 'rxjs';
 import { Game } from '../model/game.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GameService {
-  public backendUrl = 'https://ide-aabdbffdadabafcfd314190586ebabbcadeeefceacone.premiumproject.examly.io/proxy/3001/games'; 
+  public backendUrl = 'https://ide-aabdbffdadabafcfd314190586ebabbcadeeefceacone.premiumproject.examly.io/proxy/3001/games';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getGames(): Observable<Game[]> {
     return this.http.get<Game[]>(this.backendUrl);
   }
 
-  addGame(doctor: Game): Observable<Game> {
-    return this.http.post<Game>(this.backendUrl, doctor);
+  addGame(game: Game): Observable<Game> { 
+    return this.http.post<Game>(this.backendUrl, game);
   }
 
   getGameById(id: number): Observable<Game> {
     const url = `${this.backendUrl}/${id}`;
     return this.http.get<Game>(url);
   }
-  updateGame(id: number, doctor: Game): Observable<Game> {
+
+  updateGame(id: number, game: Game): Observable<Game> { 
     const url = `${this.backendUrl}/${id}`;
-    return this.http.put<Game>(url, doctor);
+    return this.http.put<Game>(url, game);
   }
 
   deleteGame(id: number): Observable<void> {
     const url = `${this.backendUrl}/${id}`;
     return this.http.delete<void>(url);
+  }
+
+  // Method to filter games by developer
+  getGamesByDeveloper(developer: string): Observable<Game[]> {
+    return this.http.get<Game[]>(`${this.backendUrl}?developer=${developer}`); 
   }
 }
