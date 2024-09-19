72de0c8858a26e563a10cce0fe1d20d7faad2593
neojarvis <techadmin@iamneo.ai>
2024-09-19 04:34:40 +0000

    Auto-save-commit-19/09/2024--10:04:38

--- GameFinder/angularapp/src/app/game-list/game-list.component.ts
+++ GameFinder/angularapp/src/app/game-list/game-list.component.ts
@@ -10,6 +10,8 @@ import { Router } from '@angular/router';
 })
 export class GameListComponent implements OnInit {
   games: Game[] = [];
+  developerName: string = '';
+  developerGameCount: number = 0;
 
   constructor(private gameService: GameService, private router: Router) {}
 
@@ -30,6 +32,25 @@ export class GameListComponent implements OnInit {
       );
     } catch (err) {
       console.log('Error:', err);
+    }
+  }
+
+  // Method to search games by developer
+  searchByDeveloper(): void {
+    if (this.developerName.trim() !== '') {
+      this.gameService.getGamesByDeveloper(this.developerName).subscribe(
+        (res) => {
+          this.games = res;
+          this.developerGameCount = res.length; // Set the count of games
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+    } else {
+      // Reset to all games if no developer is entered
+      this.getGames();
+      this.developerGameCount = 0;
     }
   }
 
