73b9d47d081998728fd92f450aa51a341e03d365
neojarvis <techadmin@iamneo.ai>
2024-10-03 04:48:04 +0000

    Auto-save-commit-03/10/2024--10:18:03

---
+++ HospitalAppointmentSchedulingSystem/dotnetapp/Migrations/20240911120637_sdfghj.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dotnetapp.Migrations
+{
+    public partial class sdfghj : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Doctors",
+                columns: table => new
+                {
+                    DoctorId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Specialty = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ContactInfo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DoctorFee = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Doctors", x => x.DoctorId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Appointments",
+                columns: table => new
+                {
+                    AppointmentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AppointmentDate = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PatientName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Reason = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DoctorId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Appointments", x => x.AppointmentId);
+                    table.ForeignKey(
+                        name: "FK_Appointments_Doctors_DoctorId",
+                        column: x => x.DoctorId,
+                        principalTable: "Doctors",
+                        principalColumn: "DoctorId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_DoctorId",
+                table: "Appointments",
+                column: "DoctorId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Appointments");
+
+            migrationBuilder.DropTable(
+                name: "Doctors");
+        }
+    }
+}
---
+++ HospitalAppointmentSchedulingSystem/dotnetapp/Models/Doctor.cs
@@ -0,0 +1,17 @@
+using System.Text.Json.Serialization;
+
+namespace dotnetapp.Models
+{
+    public class Doctor
+    {
+        public int DoctorId { get; set; }  // Primary key
+        public string Name { get; set; }  // Doctor's name
+        public string Specialty { get; set; }  // Doctor's specialty
+        public string ContactInfo { get; set; }  // Doctor's contact information
+        public int DoctorFee { get; set; }
+
+        // Navigation property for appointments scheduled with this doctor
+         [JsonIgnore]
+        public ICollection<Appointment>? Appointments { get; set; }
+    }
+}
