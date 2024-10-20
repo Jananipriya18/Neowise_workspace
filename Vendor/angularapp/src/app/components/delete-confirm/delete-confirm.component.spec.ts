69eceb8bc112c883d8f9e256cdbb6e7adbe446d9
neojarvis <techadmin@iamneo.ai>
2024-10-20 04:23:26 +0000

    Auto-save-commit-20/10/2024--09:53:24

--- Vendor/angularapp/src/app/components/delete-confirm/delete-confirm.component.spec.ts
+++ Vendor/angularapp/src/app/components/delete-confirm/delete-confirm.component.spec.ts
@@ -6,24 +6,23 @@ import { of } from 'rxjs';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { VendorService } from '../../services/vendor.service'; // Adjusted service name
-import { Vendor } from '../../models/vendor.model'; // Adjusted model name
+import { VendorService } from '../../services/vendor.service'; 
 
 describe('DeleteConfirmComponent', () => {
     let component: DeleteConfirmComponent;
     let fixture: ComponentFixture<DeleteConfirmComponent>;
     let router: Router;
     let activatedRoute: ActivatedRoute;
-    let mockVendorService: jasmine.SpyObj<VendorService>; // Adjusted service name
+    let mockVendorService: jasmine.SpyObj<VendorService>;
 
     beforeEach(waitForAsync(() => {
-        mockVendorService = jasmine.createSpyObj<VendorService>('VendorService', ['getVendor', 'deleteVendor'] as any); // Adjusted service name and methods
+        mockVendorService = jasmine.createSpyObj<VendorService>('VendorService', ['getVendor', 'deleteVendor'] as any); 
 
         TestBed.configureTestingModule({
             imports: [RouterTestingModule, HttpClientModule, FormsModule, HttpClientTestingModule],
             declarations: [DeleteConfirmComponent],
             providers: [
-                { provide: VendorService, useValue: mockVendorService } // Adjusted service name
+                { provide: VendorService, useValue: mockVendorService }
             ]
         }).compileComponents();
         router = TestBed.inject(Router);
@@ -43,11 +42,10 @@ describe('DeleteConfirmComponent', () => {
     fit('DeleteConfirmComponent_should_call_deleteVendor_method_when_confirmDelete_is_called', () => {
         const vendorId = 1; // Adjusted ID name
         
-        mockVendorService.deleteVendor.and.returnValue(of(null)); // Adjusted method name
+        mockVendorService.deleteVendor.and.returnValue(of(null)); 
 
-        (component as any).confirmDelete(vendorId); // Adjusted parameter name
-
-        expect(mockVendorService.deleteVendor).toHaveBeenCalledWith(vendorId); // Adjusted method name and parameter
+        (component as any).confirmDelete(vendorId); 
+        expect(mockVendorService.deleteVendor).toHaveBeenCalledWith(vendorId); 
     });
 });
 
