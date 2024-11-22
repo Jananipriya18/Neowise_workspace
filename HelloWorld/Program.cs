de4ce2a6d3db229c0ecab19ab55849d00dc315df
neojarvis <techadmin@iamneo.ai>
2024-11-22 00:24:55 +0000

    Auto-save-commit-22/11/2024--05:54:54

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    // Step 1: Declare a delegate
+    public delegate void MessageDelegate(string message);
+
+    // Step 2: Declare an event using the delegate
+    public static event MessageDelegate MessageEvent;
+
+    static void Main(string[] args)
+    {
+        // Step 3: Subscribe to the event
+        MessageEvent += PrintMessage;
+
+        // Step 4: Trigger the event
+        MessageEvent?.Invoke("Hello using an event!");
+    }
+
+    static void PrintMessage(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
