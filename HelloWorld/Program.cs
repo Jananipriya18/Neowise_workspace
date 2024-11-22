fd8d14010f33dc0b6b4d4fe42b8ab709c9159847
neojarvis <techadmin@iamneo.ai>
2024-11-22 07:28:36 +0000

    Auto-save-commit-22/11/2024--12:58:36

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,35 +1,56 @@
 ﻿using System;
 
-class Engine
-{
-    public void Start()
-    {
-        Console.WriteLine("Engine started.");
-    }
-}
-
-class Car
-{
-    private Engine _engine;
-
-    public Car()
-    {
-        // The Car class is responsible for creating the Engine
-        _engine = new Engine();
-    }
-
-    public void Drive()
-    {
-        _engine.Start();
-        Console.WriteLine("Car is driving.");
-    }
-}
-
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Car myCar = new Car();  // Car creates its own Engine
-        myCar.Drive();
+        int number = 25;
+
+        // If Statement
+        if (number > 20)
+        {
+            Console.WriteLine("The number is greater than 20.");
+        }
+
+        // If-Else Statement
+        if (number % 2 == 0)
+        {
+            Console.WriteLine("The number is even.");
+        }
+        else
+        {
+            Console.WriteLine("The number is odd.");
+        }
+
+        // If-Else If-Else Statement
+        if (number < 10)
+        {
+            Console.WriteLine("The number is less than 10.");
+        }
+        else if (number < 30)
+        {
+            Console.WriteLine("The number is between 10 and 30.");
+        }
+        else
+        {
+            Console.WriteLine("The number is 30 or more.");
+        }
+
+        // Switch Statement
+        switch (number)
+        {
+            case 10:
+                Console.WriteLine("The number is 10.");
+                break;
+            case 20:
+                Console.WriteLine("The number is 20.");
+                break;
+            case 25:
+                Console.WriteLine("The number is 25.");
+                break;
+            default:
+                Console.WriteLine("The number is something else.");
+                break;
+        }
     }
 }
