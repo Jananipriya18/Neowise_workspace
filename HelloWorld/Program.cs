f806eb43436d872ebc467ab4484dab69c7232b1a
neojarvis <techadmin@iamneo.ai>
2024-11-22 09:58:36 +0000

    Auto-save-commit-22/11/2024--15:28:36

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,56 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main()
     {
-        int number = 25;
+        // List Example
+        List<string> cities = new List<string> { "New York", "London", "Tokyo" };
+        cities.Add("Paris");
 
-        // If Statement
-        if (number > 20)
+        // Dictionary Example
+        Dictionary<string, string> capitals = new Dictionary<string, string>
         {
-            Console.WriteLine("The number is greater than 20.");
-        }
+            { "USA", "Washington D.C." },
+            { "UK", "London" },
+            { "Japan", "Tokyo" }
+        };
 
-        // If-Else Statement
-        if (number % 2 == 0)
-        {
-            Console.WriteLine("The number is even.");
-        }
-        else
-        {
-            Console.WriteLine("The number is odd.");
-        }
+        // Queue Example
+        Queue<int> numbersQueue = new Queue<int>();
+        numbersQueue.Enqueue(1);
+        numbersQueue.Enqueue(2);
 
-        // If-Else If-Else Statement
-        if (number < 10)
-        {
-            Console.WriteLine("The number is less than 10.");
-        }
-        else if (number < 30)
-        {
-            Console.WriteLine("The number is between 10 and 30.");
-        }
-        else
-        {
-            Console.WriteLine("The number is 30 or more.");
-        }
-
-        // Switch Statement
-        switch (number)
-        {
-            case 10:
-                Console.WriteLine("The number is 10.");
-                break;
-            case 20:
-                Console.WriteLine("The number is 20.");
-                break;
-            case 25:
-                Console.WriteLine("The number is 25.");
-                break;
-            default:
-                Console.WriteLine("The number is something else.");
-                break;
-        }
+        // Output the collections
+        Console.WriteLine("Cities: " + string.Join(", ", cities));
+        Console.WriteLine("Capitals: " + string.Join(", ", capitals.Values));
+        Console.WriteLine("Queue: " + string.Join(", ", numbersQueue));
     }
 }
